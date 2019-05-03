294700fc3e959110e6e9a69482fb107be4667719
mayconbalves <mayconmustaine@hotmail.com>
2019-05-02 21:06:00 -0300

    feat(tasks): create call to get real tasks

--- tasks_client/src/components/tasks/Tasks.js
+++ tasks_client/src/components/tasks/Tasks.js
@@ -4,16 +4,37 @@ import Col from 'react-bootstrap/Col'
 import List from './list/List'
 
 class Tasks extends Component {
+  constructor(props) {
+    super (props)
+
+    this.state = {
+      tasks: []
+    }
+
+    this.loadTasks = this.loadTasks.bind(this)
+  }
+
+
+
+  componentDidMount() {
+    this.loadTasks()
+  }
+
+  async loadTasks() {
+    let response = await fetch(`http://localhost:3001/tasks`);
+    const tasks = await response.json();
+    this.setState({ tasks: tasks });
+  }
   render() {
     return (
       <Row>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">To-do</p>
-          <List tasks={[{'title': 'Criar Header Criar Header Criar Header', 'done': false}, {'title': 'Criar footer', 'done': false}, {'title': 'Criar footer', 'done': false}]}/>
+          <List tasks={this.state.tasks.filter((task) => task.done !== true)}/>
         </Col>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">Done</p>
-          <List tasks={[{'title': 'Criar Header Criar Header Criar Header', 'done': true}, {'title': 'Criar footer', 'done': true}, {'title': 'Criar footer', 'done': true}]}/>
+          <List tasks={this.state.tasks.filter((task) => task.done === true)}/>
         </Col>
       </Row>
     );
