e6d87804b1b9fb15143f98f1db87a2707bb1cb42
mayconbalves <mayconmustaine@hotmail.com>
2022-01-27 19:16:39 -0300

    fix: lint errors

--- tasks_client/src/components/tasks/Tasks.js
+++ tasks_client/src/components/tasks/Tasks.js
@@ -1,44 +1,50 @@
-import React, { Component } from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import List from "./list/List";
-import Create from "./create/Create";
+import React, { Component } from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import List from './list/List'
+import Create from './create/Create'
 
 class Tasks extends Component {
-    constructor(props) {
-        super (props);
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            tasks: []
-        };
-
-        this.loadTasks = this.loadTasks.bind(this);
+    this.state = {
+      tasks: []
     }
 
-    componentDidMount() {
-        this.loadTasks();
-    }
+    this.loadTasks = this.loadTasks.bind(this)
+  }
 
-    async loadTasks() {
-        let response = await fetch("http://localhost:3001/tasks");
-        const tasks = await response.json();
-        this.setState({ tasks: tasks });
-    }
-    render() {
-        return (
-            <Row>
-                <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
-                    <p className="title">To-do</p>
-                    <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done !== true)} />
-                    <Create loadTasks={this.loadTasks} />
-                </Col>
-                <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
-                    <p className="title">Done</p>
-                    <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done === true)} />
-                </Col>
-            </Row>
-        );
-    }
+  componentDidMount() {
+    this.loadTasks()
+  }
+
+  async loadTasks() {
+    let response = await fetch('http://localhost:3001/tasks')
+    const tasks = await response.json()
+    this.setState({ tasks: tasks })
+  }
+  render() {
+    return (
+      <Row>
+        <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+          <p className="title">To-do</p>
+          <List
+            loadTasks={this.loadTasks}
+            tasks={this.state.tasks.filter((task) => task.done !== true)}
+          />
+          <Create loadTasks={this.loadTasks} />
+        </Col>
+        <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+          <p className="title">Done</p>
+          <List
+            loadTasks={this.loadTasks}
+            tasks={this.state.tasks.filter((task) => task.done === true)}
+          />
+        </Col>
+      </Row>
+    )
+  }
 }
 
-export default Tasks;
+export default Tasks
--- tasks_client/src/components/tasks/list/List.js
+++ tasks_client/src/components/tasks/list/List.js
@@ -1,65 +1,73 @@
-import React, { Component } from "react";
-import Card from "react-bootstrap/Card";
-import Table from "react-bootstrap/Table";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import Table from 'react-bootstrap/Table'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class List extends Component {
-    async deleteTask(task) {
-        if (window.confirm(`Are you sure you want to delete: "${task.title}"`)) {
-            await fetch(`http://localhost:3001/tasks/${task.id}`, {method: "DELETE"});
-            this.props.loadTasks();
-        }
+  async deleteTask(task) {
+    if (window.confirm(`Are you sure you want to delete: "${task.title}"`)) {
+      await fetch(`http://localhost:3001/tasks/${task.id}`, {
+        method: 'DELETE'
+      })
+      this.props.loadTasks()
     }
+  }
 
-    async checkTask(task) {
-        await fetch(`http://localhost:3001/tasks/${task.id}`,
-            {
-                method: "PUT",
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    task: { done: true }
-                })
-            }
-        );
+  async checkTask(task) {
+    await fetch(`http://localhost:3001/tasks/${task.id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        task: { done: true }
+      })
+    })
 
-        this.props.loadTasks();
-    }
-    render() {
-        return (
-            <div>
-                <Card>
-                    <Card.Body>
-                        <Table responsive>
-                            <tbody>
-                                {this.props.tasks.map((task, index) => {
-                                    return <tr key={task.id}>
-                                        <td className="col-md-10">{task.title}</td>
-                                        <td>
-                                            {
-                                                task.done === false
-                                                    ? <a className="check" href="/">
-                                                        <FontAwesomeIcon icon="check-circle" onClick={() => this.checkTask(task)} size="lg" />
-                                                    </a>
-                                                    : null
-                                            }
-                                        </td>
-                                        <td>
-                                            <a className="delete" href="/" onClick={() => this.deleteTask(task)}>
-                                                <FontAwesomeIcon icon="trash-alt"/>
-                                            </a>
-                                        </td>
-                                    </tr>;
-                                })}
-                            </tbody>
-                        </Table>
-                    </Card.Body>
-                </Card>
-            </div>
-        );
-    }
+    this.props.loadTasks()
+  }
+  render() {
+    return (
+      <div>
+        <Card>
+          <Card.Body>
+            <Table responsive>
+              <tbody>
+                {this.props.tasks.map((task) => {
+                  return (
+                    <tr key={task.id}>
+                      <td className="col-md-10">{task.title}</td>
+                      <td>
+                        {task.done === false ? (
+                          <a className="check" href="/">
+                            <FontAwesomeIcon
+                              icon="check-circle"
+                              onClick={() => this.checkTask(task)}
+                              size="lg"
+                            />
+                          </a>
+                        ) : null}
+                      </td>
+                      <td>
+                        <a
+                          className="delete"
+                          href="/"
+                          onClick={() => this.deleteTask(task)}
+                        >
+                          <FontAwesomeIcon icon="trash-alt" />
+                        </a>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  }
 }
 
-export default List;
+export default List
