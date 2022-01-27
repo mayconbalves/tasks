e649e8a426901f34c9c7f34ad68cfcf220b6579a
mayconbalves <mayconmustaine@hotmail.com>
2022-01-27 19:04:59 -0300

    feat: add prettier

--- tasks_client/src/App.js
+++ tasks_client/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Tasks from './components/tasks/Tasks'
+import React from "react";
+import Header from "./components/header/Header";
+import Tasks from "./components/tasks/Tasks";
 
-import Container from 'react-bootstrap/Container'
+import Container from "react-bootstrap/Container";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
-library.add(faCheckCircle, faTrashAlt)
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCheckCircle, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+library.add(faCheckCircle, faTrashAlt);
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Container>
-        <Tasks />
-      </Container>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Container>
+                <Tasks />
+            </Container>
+        </div>
+    );
 }
 
 export default App;
