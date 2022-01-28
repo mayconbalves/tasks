fe627dc3264e8715de203a53acdac4e3217cda47
mayconbalves <mayconmustaine@hotmail.com>
2022-01-27 21:30:45 -0300

    refactor: remove css files

--- tasks_client/src/index.js
+++ tasks_client/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { createGlobalStyle } from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: "Open Sans", Helvetica, sans-serif, arial;
+    margin: 0;
+    padding: 0;
+  }
+`
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root')
+)
 serviceWorker.unregister()
